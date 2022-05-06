eed9bd466f97e9ea9e16ccd8eaaf6a8ffb6054ed
Diazgerard <gerardourraco234@gmail.com>
2022-05-06 13:28:24 -0600

    Incio de proyecto

---
+++ src/gerardodiaz_lab3p2/GerardoDiaz_Lab3P2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gerardodiaz_lab3p2;
+
+/**
+ *
+ * @author gerar
+ */
+public class GerardoDiaz_Lab3P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
